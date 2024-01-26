ccd7314508a3827491e94e220d00bb9fcc5cff85
Tris <gingertris1@gmail.com>
2024-01-26 15:36:25 +0000

    player name enter and select

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
     let editMode = false;
-    const playerNames:string[] = ["","","","","",""];
+    const playerNames:string[] = ["Player 1","Player 2","Player 3","Player 4","Player 5","Player 6"];
     let selectedId: number = 0;
 
     const onKeyDown = (e:KeyboardEvent) => {
         //@ts-expect-error
         const targetId: string|undefined = e.target?.id;
+
         if(targetId?.includes("player_input")) return editMode=true;
         editMode=false;
 
         console.log(e);
-        console.log(playerNames);
 
         if(["1","2","3","4","5","6"].includes(e.key)){
             selectedId = parseInt(e.key) - 1;
@@ -24,28 +24,28 @@
         editMode=false;
     }
 
+
 </script>
 
 <svelte:window on:keydown={onKeyDown} on:click={onClick}/>
 <div class="p-3 flex flex-col space-y-5">
     <div id="players" class="flex space-x-3">
         {#each [0,1,2,3,4,5] as i}
-        <div class="flex-auto grid grid-cols-1 gap-y-2">
-            <div class="col-auto text-center  {i == selectedId ? "bg-green-400" : "" }">Player {i+1}</div>
-            <div class="outline col-auto">
-                <input class="w-full" id="{`player_input_${i}`}" type="text" bind:value={playerNames[i]} placeholder="Enter name">
+        <div class="flex-auto">
+            <div class="outline-dashed">
+                <input class="w-full p-1 {i == selectedId ? "bg-green-400" : "" }" id="{`player_input_${i}`}" type="text" bind:value={playerNames[i]}>
             </div>
         </div>
             
         {/each}
     </div>
     
-    <div class="flex flex-col space-y-2">
-        <div class="outline p-2">
+    <div class="flex flex-col space-y-2" id="events">
+        <div class="outline p-2" id="currentevent">
             <h2 class="underline">Current Event</h2>
             {#if editMode}
                 <div>
-                    Currently editing, not recording keypresses. Click on me to resume recording keypresses.
+                    Currently editing, not recording keypresses. Click elsewhere to resume recording keypresses.
                 </div>
             {:else}
                 <div>
@@ -56,8 +56,8 @@
         
         
         
-        <div class="outline p-2">
-            <h2 class="underline">Events</h2>
+        <div class="outline p-2" id="eventlog">
+            <h2 class="underline">Event Log</h2>
         </div>
     </div>
     
