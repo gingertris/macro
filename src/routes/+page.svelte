0368178793ecebf8dd0a06a764922120e5a3a5aa
Tris <gingertris1@gmail.com>
2024-01-26 15:18:34 +0000

    basic ui done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     let editMode = false;
     const playerNames:string[] = ["","","","","",""];
-    const selectedId: number = 0;
+    let selectedId: number = 0;
 
     const onKeyDown = (e:KeyboardEvent) => {
         //@ts-expect-error
@@ -11,6 +11,11 @@
 
         console.log(e);
         console.log(playerNames);
+
+        if(["1","2","3","4","5","6"].includes(e.key)){
+            selectedId = parseInt(e.key) - 1;
+        }
+
     }
     const onClick = (e:MouseEvent) => {
         //@ts-expect-error
@@ -22,33 +27,38 @@
 </script>
 
 <svelte:window on:keydown={onKeyDown} on:click={onClick}/>
-
-<div id="players" class="flex space-x-3 p-3">
-    {#each [0,1,2,3,4,5] as i}
-    <div class="flex-auto grid grid-cols-1 gap-y-2">
-        <div class="col-auto text-center">Player {i}</div>
-        <div class="outline col-auto">
-            <input class="w-full" id="{`player_input_${i}`}" type="text" bind:value={playerNames[i]} placeholder="Enter name">
+<div class="p-3 flex flex-col space-y-5">
+    <div id="players" class="flex space-x-3">
+        {#each [0,1,2,3,4,5] as i}
+        <div class="flex-auto grid grid-cols-1 gap-y-2">
+            <div class="col-auto text-center  {i == selectedId ? "bg-green-400" : "" }">Player {i+1}</div>
+            <div class="outline col-auto">
+                <input class="w-full" id="{`player_input_${i}`}" type="text" bind:value={playerNames[i]} placeholder="Enter name">
+            </div>
+        </div>
+            
+        {/each}
+    </div>
+    
+    <div class="flex flex-col space-y-2">
+        <div class="outline p-2">
+            <h2 class="underline">Current Event</h2>
+            {#if editMode}
+                <div>
+                    Currently editing, not recording keypresses. Click on me to resume recording keypresses.
+                </div>
+            {:else}
+                <div>
+                    <p>Target: {playerNames[selectedId]}</p>
+                </div>
+            {/if}
+        </div>
+        
+        
+        
+        <div class="outline p-2">
+            <h2 class="underline">Events</h2>
         </div>
     </div>
-        
-    {/each}
-</div>
-
-<div>
-    <h3>Current Event</h3>
-    {#if editMode}
-        <div>
-            Currently editing, not recording keypresses. Click on me to resume recording keypresses.
-        </div>
-    {:else}
-        <div>
-            
-        </div>
-    {/if}
-</div>
-
-
-<div>
-    <h3>Events</h3>
+    
 </div>
