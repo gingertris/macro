15b092843275e4bef6c943b53e1ca5bb612a5648
Tris <gingertris1@gmail.com>
2024-02-01 15:34:37 +0000

    completely gut eventhandler

--- src/lib/eventHandler.ts
+++ src/lib/eventHandler.ts
@@ -6,17 +6,17 @@ type Event = {
     opponentTeamName: string,
     playerName: string,
     boost: number,
-    X: number,
-    Y: number,
-    Z: number,
+    position: Position
     event: string,
     outcome: string, 
-    secondaryX?: number,
-    secondaryY?: number,
-    secondaryZ?: number,
-    secondaryPlayer?: number
+    secondary?: Position | string
 }
 
+type Position = {
+    X: number, 
+    Y: number,
+    Z: number
+}
 
 const getTeamName = (playerId: number) => {
     const $customTeamNames = get(customTeamNames);
@@ -43,36 +43,11 @@ const getContext = () => {
 
 
 export const parseEventCode = (eventCode: string[], playerData: any) => {
-
-    const context = getContext();
-    const preHandledEvent = {
-        ...context,
-        boost: playerData.boost,
-        X: playerData.location.X,
-        Y: playerData.location.Y,
-        Z: playerData.location.Z,
-    }
-
     eventCode = eventCode.map(c => c.toLowerCase());
 
     if(eventCode.length === 0) throw new Error("Empty");
 
-    switch(eventCode.shift()){
-        case "s": //shot
-            return handleShot(eventCode, playerData)
-        case "f": // 50-50
-            return handleFifty(eventCode, playerData);
-        case "u": //uncontested
-            return handleUncontested(eventCode, playerData);
-        case "g": //goal
-            return handleGoal(eventCode, playerData);
-        case "b": //first block
-            return handleBlock(eventCode, playerData);
-    }
-
-}
-
-const handleFifty = (eventCode: string[], playerData: any) => {
+    return 
 
 }
 
@@ -84,10 +59,22 @@ const handleGoal = (eventCode: string[], playerData: any) => {
 
 }
 
+const handleShot = (eventCode: string[], playerData: any) => {
+
+}
+
 const handleUncontested = (eventCode: string[], playerData: any) => {
 
 }
 
-const handleShot = (eventCode: string[], playerData: any) => {
-
+const handleFifty = (eventCode: string[], playerData: any) => {
+    return {
+        eventName: "50/50",
+        outcome: "good",
+        secondary: {
+            X: 1,
+            Y: 2,
+            Z: 3
+        }
+    }
 }
